5d2047fc71991c3fce15d2f668751e950bc20b8c
Adam Bordelon <freddyyeddy@gmail.com>
2021-05-03 23:45:08 -0800

    Add files via upload
    This is the current state of the conversion/upgrade

---
+++ Modified Fiels/resource.h
@@ -0,0 +1,75 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by Duell.rc
+//
+#define IDI_MAIN                        101
+#define IDR_MENU                        102
+#define IDR_ACCEL                       103
+#define IDD_HILFE                       124
+#define IDD_DUELL                       129
+#define IDB_DUELL                       130
+#define IDD_ALERT                       134
+#define IDD_THINORML                    137
+#define IDD_THIAHEAD                    138
+#define IDD_ONE                         139
+#define IDD_TWO                         140
+#define IDD_NONE                        141
+#define IDD_WHITE                       142
+#define IDD_BLACK                       143
+#define IDD_BACKWARD                    144
+#define IDD_WHATSDUELL                  145
+#define IDD_MOVEXAMP                    147
+#define IDD_RANDOM                      148
+#define IDD_THRESHOLDS                  149
+#define IDD_SHOWEINSTELLUNG             150
+#define IDM_EXIT                        1001
+#define IDC_WEITERE_INFO                1018
+#define IDC_THRES1                      1026
+#define IDC_THRES2                      1027
+#define IDC_STANDARD                    1028
+#define IDC_STUFE1                      1029
+#define IDC_STUFE2                      1030
+#define IDC_STUFE3                      1031
+#define IDC_INFOTEXT1                   1032
+#define IDC_INFOTEXT2                   1033
+#define IDC_INFOTEXT3                   1034
+#define IDC_STR                         5040
+#define IDC_YESYES                      5041
+#define IDC_NONO                        5042
+#define IDM_TEST                        40024
+#define ID_DUELL_NEUESSPIEL             40025
+#define ID_DUELL_SOUND                  40027
+#define ID_INFO_HILFE                   40029
+#define ID_INFO_DUELL                   40031
+#define IDM_DOWN                        40033
+#define ID_THINORML                     40040
+#define ID_THIAHEAD                     40041
+#define ID_ONE                          40042
+#define ID_TWO                          40043
+#define ID_NONE                         40044
+#define ID_WHITE                        40045
+#define ID_BLACK                        40046
+#define ID_BACKWARD                     40047
+#define ID_WHATSDUELL                   40048
+#define ID_SPIEL_SPEICHERN              40049
+#define ID_SPIEL_LADEN                  40050
+#define ID_WRITEPROT                    40051
+#define ID_READPROT                     40052
+#define ID_RULES                        40053
+#define ID_MOVEXAMP                     40054
+#define ID_RANDOM                       40055
+#define ID_THRESHOLDS                   40056
+#define ID_SHOWEINSTELLUNG              40057
+#define ID_DUELL_MARKERS                40058
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        152
+#define _APS_NEXT_COMMAND_VALUE         40059
+#define _APS_NEXT_CONTROL_VALUE         1035
+#define _APS_NEXT_SYMED_VALUE           104
+#endif
+#endif
